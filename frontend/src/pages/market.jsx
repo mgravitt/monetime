bda36fe1b911514b991c2d1aad8f6368e41c441c
Philip Dhingra <philipkd@gmail.com>
2018-11-11 10:53:58 -0800

    added charities

--- frontend/src/pages/market.jsx
+++ frontend/src/pages/market.jsx
@@ -11,18 +11,18 @@ class Market extends Component {
     return (
       <div id="Market">
         <section>
-          <h2>Real-World Services</h2>
+          <h2>Apps</h2>
           <AppIcon name="24-Hour Fitness" symbol="24h" handlePageChange={this.props.handlePageChange} />
-          <AppIcon name="HBO" symbol="hbo" handlePageChange={this.props.handlePageChange} />
+          <AppIcon name="World of Warcraft" symbol="wow" handlePageChange={this.props.handlePageChange} />
           <AppIcon name="New York Times" symbol="nyt" handlePageChange={this.props.handlePageChange} />
           <AppIcon name="Verizon" symbol="verizon" handlePageChange={this.props.handlePageChange} />
         </section>
         <section>
-          <h2>Games</h2>
-          <AppIcon name="World of Warcraft" symbol="wow" handlePageChange={this.props.handlePageChange} />
-          <AppIcon name="Angry Birds" symbol="birds" handlePageChange={this.props.handlePageChange} />
-          <AppIcon name="Assassin's Creed" symbol="creed" handlePageChange={this.props.handlePageChange} />
-          <AppIcon name="Minecraft" symbol="minecraft" handlePageChange={this.props.handlePageChange} />
+          <h2>Charities</h2>
+          <AppIcon name="Red Cross" symbol="redcross" handlePageChange={this.props.handlePageChange} />
+          <AppIcon name="Susan G. Komen" symbol="komen" handlePageChange={this.props.handlePageChange} />
+          <AppIcon name="Unicef" symbol="unicef" handlePageChange={this.props.handlePageChange} />
+          <AppIcon name="USO" symbol="uso" handlePageChange={this.props.handlePageChange} />
         </section>
         {/*<section>
           <h2>Productivity</h2>
